559797c05dd711a1a2b86919784278584225d719
buildingSMART-Japan <technical_committee@building-smart.or.jp>
2025-12-05 03:53:18 +0000

    translate from buildingSMART/validate

---
+++ backend/apps/ifc_validation/api/v1/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+
+from apps.ifc_validation_models.models import ValidationRequest
+from apps.ifc_validation_models.models import ValidationTask
+from apps.ifc_validation_models.models import ValidationOutcome
+from apps.ifc_validation_models.models import Model
+
+from .pydantic_bridge import PydanticValidatorMixin
+from .schemas import ValidationRequestIn
+
+class BaseSerializer(serializers.ModelSerializer):
+
+    def get_field_names(self, declared_fields, info):
+
+        # Django does not support both 'fields' and 'exclude'
+
+        expanded_fields = super(BaseSerializer, self).get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, 'show', None):
+            expanded_fields = expanded_fields + self.Meta.show
+
+        if getattr(self.Meta, 'hide', None):
+            expanded_fields = list(set(expanded_fields) - set(self.Meta.hide))
+        
+        return expanded_fields
+
+    class Meta:
+        abstract = True
+        
+
+class ValidationRequestSerializer(PydanticValidatorMixin, BaseSerializer):
+
+    Schema = ValidationRequestIn
+    
+    class Meta:
+        model = ValidationRequest
+        fields = '__all__'
+        show = ["public_id", "model_public_id"]
+        hide = ["id", "model", "deleted", "created_by", "updated_by", "status_reason"]
+        read_only_fields = ['size', 'created', 'updated', 'created_by', 'updated_by', 'channel', 'completed', 'started', 'progress', 'status']
+
+
+class ValidationTaskSerializer(PydanticValidatorMixin, BaseSerializer):
+    
+    class Meta:
+        model = ValidationTask
+        fields = '__all__'
+        show = ["public_id", "request_public_id"]
+        hide = ["id", "process_id", "process_cmd", "request"]
+
+
+class ValidationOutcomeSerializer(PydanticValidatorMixin, BaseSerializer):
+    
+    class Meta:
+        model = ValidationOutcome
+        fields = '__all__'
+        show = ["public_id", "instance_public_id", "validation_task_public_id"]
+        hide = ["id", "instance", "validation_task"]
+
+
+class ModelSerializer(PydanticValidatorMixin, BaseSerializer):
+
+    class Meta:
+        model = Model
+        fields = '__all__'
+        show = ["public_id"]
+        hide = ["id", "number_of_elements", "number_of_geometries", "number_of_properties"] # no longer used
+        read_only_fields = ['',]
